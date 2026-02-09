e3c6ea3da6b007cb902a4408a27e8d7bc5e3cddf
ctkoth <ctkoth@gmail.com>
2026-02-09 17:21:49 -0500

    idk

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${domain}/api/auth/google/callback`
+    callbackURL: "https://musicconnectz.net/auth/google/callback"
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       const users = await readUsers();
