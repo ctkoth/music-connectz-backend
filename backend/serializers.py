71503092b890192baf549bac66a2bf54c78e50f7
ctkoth <ctkoth@gmail.com>
2026-03-29 21:11:19 -0400

    Fix: move serializers import to top

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from .models import CollabReliabilityRating, CollabReview
 # --- Reliability Rating Serializer ---
 class CollabReliabilityRatingSerializer(serializers.ModelSerializer):
