caa3090a4beaba4b8d947c475d3a8a2b8bd50adb
ctkoth <ctkoth@gmail.com>
2026-06-07 16:59:19 -0400

    Add apps/accounts app

---
+++ apps/accounts/views.py
@@ -0,0 +1,5 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+class HealthView(APIView):
+    def get(self, request):
+        return Response({"status": "ok"})
